74871a1c84e0e5f16443ae83e211e8a29f9b9ed0
hab <Mohabzax4@gmail.com>
2022-12-11 23:18:35 +0200

    add resources

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:topics/Pages/splash_page.dart';
+import 'package:topics/Resources/routes_manager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splash,
+      onGenerateRoute: RouteGenerator.getRoute,
       home: SplashPage(),
     );
   }
